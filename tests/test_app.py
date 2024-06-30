f4876ce16458806643cb20ca85dd3b6ce3d538b7
Helton Teixeira de Souza <heltonteixeiradesouza@hotmail.com>
2024-06-30 18:05:18 -0300

    Implementando rotas CRUD

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,14 +1,60 @@
 from http import HTTPStatus
 
-from fastapi.testclient import TestClient
 
-from fast_zero.app import app
+def test_create_user(client):
+    # client = TestClient(app)  # Arrange
+
+    data = {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'password': 'secret',
+    }
+
+    response = client.post('/users', json=data)  # Act
+
+    assert response.status_code == HTTPStatus.CREATED  # Assert
+    assert response.json() == {
+        'username': 'alice',
+        'email': 'alice@example.com',
+        'id': 1,
+    }
 
 
-def test_root_status_ok_and_hello_world_msg():
-    client = TestClient(app)  # Arrange
+def test_read_users(client):
+    response = client.get('/users/')
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'users': [
+            {
+                'username': 'alice',
+                'email': 'alice@example.com',
+                'id': 1,
+            }
+        ]
+    }
 
-    response = client.get('/')  # Act
+    assert response
 
-    assert response.status_code == HTTPStatus.OK  # Assert
-    assert response.json() == {'message': 'Hello world!'}  # Assert
+
+def test_update_user(client):
+    data = {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'password': 'mynewpassword',
+    }
+
+    response = client.put('/users/1', json=data)
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {
+        'username': 'bob',
+        'email': 'bob@example.com',
+        'id': 1,
+    }
+
+
+def test_delete_user(client):
+    response = client.delete('/users/1')
+
+    assert response.status_code == HTTPStatus.OK
+    assert response.json() == {'message': 'User deleted'}
---
+++ utils.py
@@ -0,0 +1,8 @@
+import socket
+
+
+def get_your_ip_address():
+    """just a curiosity"""
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    s.connect(('8.8.8.8', 80))
+    return s.getsockname()[0]
